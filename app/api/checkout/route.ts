820ac7a0d4ac68af16503bffaf671cc43ec830ba
Eren Bulur <bulureren76@gmail.com>
2026-03-30 03:46:12 +0300

    fix stripe init

--- app/api/checkout/route.ts
+++ app/api/checkout/route.ts
@@ -1,28 +1,26 @@
 import { NextResponse } from "next/server";
 import Stripe from "stripe";
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
-
 export async function POST(req: Request) {
   try {
+    // 👇 STRIPE BURAYA TAŞINDI
+    const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string);
+
     const body = await req.json();
     const email = body.email;
 
     const session = await stripe.checkout.sessions.create({
       payment_method_types: ["card"],
       mode: "payment",
-
-      customer_email: email, // 🔥 BURASI EMAIL BAĞLIYOR
-
+      customer_email: email,
       line_items: [
         {
           price: "price_1TG0inRwWZhiueDMlPYQJDNP",
           quantity: 1,
         },
       ],
-
-      success_url: "http://localhost:3000/dashboard?success=1",
-      cancel_url: "http://localhost:3000/dashboard?canceled=1",
+      success_url: "http://localhost:3000",
+      cancel_url: "http://localhost:3000",
     });
 
     return NextResponse.json({ url: session.url });
